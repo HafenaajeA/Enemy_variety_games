f0c94f27582bfc8491839d263b23bf165f845906
Almando <almandocode@gmail.com>
2022-08-13 22:45:33 +0200

    y first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 500;
+    canvas.height = 800;
+
+class Game {
+    constructor(){
+        this.enemy= [];
+
+
+    }
+    update(){
+
+    }
+    draw(){
+
+    }
+    #addNewEnemy(){
+
+    }
+}
+
+class Enemy{
+    constructor(){
+
+    }
+    update(){
+
+    }
+    draw(){
+
+    }
+}
+let lastTime = 1;
+function animate(timeStamp){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+
+    requestAnimationFrame(animate)
+};
+animate(0);
+});
